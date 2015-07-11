d4ee288f0767a0fc647233725611607fd6e9ebb0
psvmc <183518918@qq.com>
2015-07-11 12:11:42 +0800

    .

--- jekyll-themes/ztheme/js/randomColor.js
+++ jekyll-themes/ztheme/js/randomColor.js
@@ -1,5 +1,5 @@
 function randomColor() {
-	var colorArray = ["#FF9966","#FF6666","#FFCCCC","#FFCC99","#99CC99"];
+	var colorArray = ["#FF9966","#FF6666","#FFCCCC","#FFCC99","#99CC99","#6FB934","#4899E0"];
 	var tempColor = 0;
 	var randomNum = 0;
 	$(".main-article li").each(function(i, item) {
