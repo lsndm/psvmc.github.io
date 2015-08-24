67757b6a7fa01b4c05d1a8c3b5bd8d7c0ec5310f
psvmc <183518918@qq.com>
2015-08-24 09:44:36 +0800

    腾讯地图

---
+++ zjdemo/tencentMap/tencentMap.js
@@ -0,0 +1,162 @@
+var map = null;
+var drivingService = null;
+var isFirst = 0;
+//开始结束图标
+var startMarker;
+var endMarker;
+var mystartPosition = new qq.maps.LatLng(34.8068225119258, 113.693930500652);
+var myendPosition;
+
+function jumpTwo() {
+	var $iMarker = $("#iMarker");
+	var yuanTop = $iMarker.position().top;
+	var newTop = parseInt(yuanTop) - 15;
+
+	$iMarker.animate({
+		top: newTop,
+	}, 300, "linear", function() {
+		$iMarker.animate({
+			top: yuanTop,
+		}, 300, "linear", function() {
+			$iMarker.animate({
+				top: newTop,
+			}, 300, "linear", function() {
+				$iMarker.animate({
+					top: yuanTop,
+				}, 300, "linear");
+			});
+		});
+	});
+}
+
+function removeLogo() {
+	var logo = $(".csssprite").closest("a").closest("div");
+	logo.css("display", "none");
+	logo.next("div").css("display", "none");
+
+}
+
+function init() {
+	var center = new qq.maps.LatLng(34.8068225119258, 113.693930500652);
+	map = new qq.maps.Map(document.getElementById('container'), {
+		center: center,
+		zoom: 12,
+		mapTypeControl: false
+	});
+	var controlStyle = "widht:52px;height:52px";
+	var centerControl = document.createElement("div");
+	centerControl.id = "iMarker"
+	centerControl.style.cssText = controlStyle;
+	centerControl.innerHTML = "<image src='marker.png' height='100%' width='100%' />";
+	centerControl.index = 1; //设置在当前布局中的顺序
+	map.controls[qq.maps.ControlPosition.CENTER].push(centerControl);
+
+
+	var geocoder = new qq.maps.Geocoder({
+		complete: function(result) {
+			var nearPois = result.detail.nearPois;
+
+			var $dizhiList = $("#dizhiList");
+			$dizhiList.html("");
+
+
+			for (var i = 0; i < nearPois.length; i++) {
+				var itemlatLng = nearPois[i].latLng;
+				var $li = $("<li />")
+				var $divTitle = $("<div />");
+				$divTitle.attr("class", "dizhiTitle");
+				$divTitle.html(nearPois[i]["name"]);
+				var $divDetail = $("<div />");
+				$divDetail.attr("class", "dizhiDetail");
+				$divDetail.html(nearPois[i]["name"]);
+
+				var $dizhiLeft1 = $("<div />");
+				$dizhiLeft1.attr("class", "dizhiLeft");
+				$dizhiLeft1.append($divTitle);
+				$dizhiLeft1.append($divDetail);
+
+				var $dizhiRight1 = $("<div />");
+				$dizhiRight1.attr("class", "dizhiRight");
+
+				$li.append($dizhiLeft1);
+				$li.append($dizhiRight1);
+
+				var $itemlat = $("<input type='hidden' class='itemlat' />")
+				$itemlat.val(itemlatLng.getLat());
+
+				var $itemlng = $("<input type='hidden' class='itemlng' />")
+				$itemlng.val(itemlatLng.getLng());
+
+				$li.append($itemlat);
+				$li.append($itemlng);
+				$dizhiList.append($li);
+			}
+
+			$dizhiList.find("li").click(function() {
+				$(this).closest("ul").find(".dizhiRight").each(function(item, obj) {
+					$(obj).removeClass("checkedImage");
+				});
+				console.info($(this).find(".dizhiDetail").html());
+				$(this).find(".dizhiRight").addClass("checkedImage");
+
+			})
+
+			document.getElementById('end').value = result.detail.address;
+		}
+	});
+	qq.maps.event.addListener(map, "tilesloaded", function() {
+		removeLogo();
+		var centerPoint = map.getCenter();
+		geocoder.getAddress(centerPoint)
+	});
+
+	qq.maps.event.addListener(map, "mouseover", function() {
+		removeLogo();
+	});
+	qq.maps.event.addListener(map, 'click', function(event) {
+		myendPosition = event.latLng;
+		if (endMarker) {
+			endMarker.setPosition(event.latLng)
+		} else {
+			endMarker = new qq.maps.Marker({
+				position: event.latLng,
+				map: map
+			});
+		}
+		geocoder.getAddress(event.latLng)
+	});
+	qq.maps.event.addListener(map, "dragend", function() {
+		yuanlaiTop = document.getElementById("iMarker").offsetTop;
+		var centerPoint = map.getCenter();
+
+		jumpTwo();
+		geocoder.getAddress(centerPoint)
+	});
+
+	//自动补全地址
+	var ap = new qq.maps.place.Autocomplete(document.getElementById('start'));
+	var aq = new qq.maps.place.Autocomplete(document.getElementById('end'));
+	//设置路线规划
+	drivingService = new qq.maps.DrivingService({
+		location: "郑州",
+		map: map
+	});
+
+}
+
+function getResult() {
+	//设置searchRequest
+	var jvli = document.getElementById("jvli")
+	drivingService.search(mystartPosition, myendPosition);
+	//设置回调函数
+	drivingService.setComplete(function(result) {
+		if (result.type == qq.maps.ServiceResultType.MULTI_DESTINATION) {
+			alert("起终点不唯一");
+		}
+	});
+	//获取距离
+	drivingService.setComplete(function(result) {
+		console.info(result.detail.distance);
+		jvli.innerHTML = result.detail.distance;
+	});
+}
